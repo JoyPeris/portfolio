3703c0d791271afdb51b6350487c856e3748c49b
Peris <joyperis11@gmail.com>
2024-09-22 05:50:36 +0300

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,13 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'portfolio';
+  phoneNumber: string = '+254 719 724 004';
+
+  copyToClipboard(text: string) {
+    navigator.clipboard.writeText(text).then(() => {
+      alert(`Phone number copied to clipboard: ${text}`);
+    }).catch(err => {
+      console.error('Failed to copy: ', err);
+    });
+  }
 }
